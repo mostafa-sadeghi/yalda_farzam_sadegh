7527ea33e69d579cb1fa4643a87c2faa7dd3d885
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-28 19:58:33 +0330

    sadeghi

--- s2.py
+++ s2.py
@@ -1,1 +1,55 @@
-print(1+2)
+#salaam be hame 
+#به کلاس پایتون خوش آمدید
+# print(1+2)
+# print("1+2") 
+# print("Hello")
+# print("artin")
+# print('artin')
+# print("sadegh", "yalda", "farzam")
+# print("Your score is:", 100+10)
+# print("Your score is:", "100+10", "balalalall")
+# print("Your new score is:", 100)
+
+# print("I want to print a double quote \" for some reason")
+
+# print("The file is stored in C:\new folder")
+
+# برنامه ای بنویسید که نام سه نفر از دوستانتان را پرینت نماید
+# برنامه ای بنویسید که سه عدد را پرینت نماید
+# برنامه ای بنویسید که مجموع سه عدد را پرینت نماید
+
+
+
+# x = 1
+# print(x)
+
+# x = x + 1
+# print(x)
+
+
+# a = 10
+# b = 5
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+
+number1 = 5
+number2 = 2
+
+print(number1 / number2)
+print(number1 // number2)# تقسیم صحیح
+
+print(number1 % number2)
+
+
+z = 1 + 2 * 3
+print(z)
+z = (1 + 2) * 3
+print(z)
+
+average = 90 + 86 + 71 + 100 + 98 / 5
+print(average)
+# why ???
