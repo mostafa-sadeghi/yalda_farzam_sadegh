cc270bf4c7e46d3ee8fc374ae897e79cc100ee85
msa <msai.sadehi@gmail.com>
2023-07-09 20:01:20 +0330

    sadeghi

---
+++ s8.py
@@ -0,0 +1,43 @@
+# user_name = input("Enter the username: ")
+
+# if user_name == "artin":
+#     print("ok")
+
+# else:
+#     print("not ok")
+
+
+# for i in range(3):
+#     print("a")
+
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+
+
+#     for j in range(3):
+#         print("b")
+
+
+for i in range(3):
+    print("a")
+
+
+i = 0
+while i < 3:
+    print("a")
+    i += 1
+
+
+for i in range(5):
+    print(i)
+
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
