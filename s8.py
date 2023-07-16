c3e209093a81e643bb4fb1b04dd4173186280e05
msa <msai.sadehi@gmail.com>
2023-07-16 19:58:11 +0330

    while loop

--- s8.py
+++ s8.py
@@ -1,8 +1,8 @@
 # user_name = input("Enter the username: ")
-
+#
 # if user_name == "artin":
 #     print("ok")
-
+#
 # else:
 #     print("not ok")
 
@@ -22,22 +22,22 @@
 #     for j in range(3):
 #         print("b")
 
-
-for i in range(3):
-    print("a")
-
-
-i = 0
-while i < 3:
-    print("a")
-    i += 1
-
-
-for i in range(5):
-    print(i)
-
-
-i = 0
-while i < 5:
-    print(i)
-    i += 1
+#
+# for i in range(3):
+#     print("a")
+#
+#
+# i = 0
+# while i < 3:
+#     print("a")
+#     i += 1
+#
+#
+# for i in range(5):
+#     print(i)
+#
+#
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
---
+++ session9.py
@@ -0,0 +1,41 @@
+# user_name = input("enter the username: ")
+# count = 0
+#
+# while user_name != 'sara' and count < 2:
+#     user_name = input("username is not valid, please enter correct username: ")
+#     count += 1
+# if user_name == 'sara':
+#     print("Login success")
+#     print(f"welcome {user_name}")
+# else:
+#     print(f"{count + 1} times you entered invalid username and you are blocked.")
+
+
+# quit = "n"
+# while quit == "n":
+#     quit = input("Do you want to quit? ")
+# print("good bye")
+
+
+
+done = False
+while not done:
+    quit = input("Do you want to quit? ")
+    if quit == "y":
+        done = True
+
+    attack = input("Does your elf attack the dragon? ")
+    if attack == "y":
+        print("Bad Choice, you died.")
+        done = True
+
+
+# TODO    با کمک حلقه فور و نیز حلقه وایل تمامی کاراکترهای متن زیر را چداگانه پرینت نمائید
+
+message = "We want to learn pygame"
+
+# print(message[0])
+print(len(message))
+
+
+# TODO    با کمک حلقه فور و نیز حلقه وایل، برنامه ای بنویسید که مجموع اعداد کمتر از 100 را محاسبه نمایش دهد
