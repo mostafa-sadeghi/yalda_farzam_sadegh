333922184ebeaee1f1550e49de39d4469d14c76c
msa <msai.sadehi@gmail.com>
2023-07-23 19:57:50 +0330

    rock

---
+++ session10.py
@@ -0,0 +1,69 @@
+import turtle
+
+# main_screen = turtle.Screen()
+# main_screen.bgcolor("cyan")
+# main_screen.bgpic("flappyBird.png")
+# main_screen.setup(600, 600)
+# main_screen.title("our app")
+#
+# sides = int(main_screen.textinput("user input", "how many sides do you want? "))
+#
+# my_turtle = turtle.Turtle()
+# my_turtle.speed("slowest")
+# my_turtle.shape("turtle")
+# my_turtle.shapesize(3)
+# my_turtle.pensize(3)
+# my_turtle.pencolor("red")
+# my_turtle.fillcolor("green")
+
+#  کشیدن دایره
+# my_turtle.circle(150)
+#
+# my_turtle.begin_fill()
+# for i in range(sides):
+#     my_turtle.forward(150)
+#     my_turtle.left(360/sides)
+# my_turtle.end_fill()
+
+# my_turtle.ht()
+
+# TODO
+"""
+کشیدن مثلث
+کشیدن
+پنجضلعی
+کشیدن شش ضلعی
+کشیدن ستاره
+
+تمام اشکال بالا با رنگ انتخابی کاربر کشیده شوند
+
+کشیدن دایره توپر با رنگ دلخواه
+یعنی کاربر رنگ مورد نظرش را وارد نماید
+اندازه دایره نیز به انتخاب کاربر باشه
+
+
+
+"""
+
+
+
+
+# turtle.done()
+
+
+
+# import random
+#
+#
+# for i in range(5):
+#     x = random.randrange(10)
+#     print(x)
+
+user_choices = ["rock", "paper", "scissors"]
+
+
+# TODO   بازی سنگ کاغذ قیچی
+"""
+انتخاب کاربر با تابع input  گرفته شود
+انتخاب کامپیوتر به صورت random باشد
+"""
--- session9.py
+++ session9.py
@@ -18,24 +18,32 @@
 
 
 
-done = False
-while not done:
-    quit = input("Do you want to quit? ")
-    if quit == "y":
-        done = True
-
-    attack = input("Does your elf attack the dragon? ")
-    if attack == "y":
-        print("Bad Choice, you died.")
-        done = True
+# done = False
+# while not done:
+#     quit = input("Do you want to quit? ")
+#     if quit == "y":
+#         done = True
+#
+#     attack = input("Does your elf attack the dragon? ")
+#     if attack == "y":
+#         print("Bad Choice, you died.")
+#         done = True
 
 
 # TODO    با کمک حلقه فور و نیز حلقه وایل تمامی کاراکترهای متن زیر را چداگانه پرینت نمائید
 
 message = "We want to learn pygame"
 
+
+i = 0
+while i < len(message):
+    print(message[i], end=" ")
+    i += 1
+
+
+
 # print(message[0])
-print(len(message))
+# print(len(message))
 
 
 # TODO    با کمک حلقه فور و نیز حلقه وایل، برنامه ای بنویسید که مجموع اعداد کمتر از 100 را محاسبه نمایش دهد
