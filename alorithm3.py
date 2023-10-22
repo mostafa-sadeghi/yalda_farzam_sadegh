3599df94188a6b9e3eea08b008fabc135bd52fba
msa <msai.sadeghi@gmail.com>
2023-10-22 19:58:08 +0330

    s

---
+++ alorithm3.py
@@ -0,0 +1,20 @@
+# numbers = (1, 2, 3, 33, 400, 8, 14, 24)
+
+# for number in numbers:
+#     if number % 2 == 0 and number != 2:
+#         print(number)
+
+# همه اعداد زوجی که رقم دو ندارند
+
+
+numbers = (1, 2, 3, 33, 400, 8, 14, 24)
+for number in numbers:
+    if number % 2 == 0:
+        if "2" not in str(number):
+            print(number)
+
+# TODO
+message = "salaam khobi?"
+# با کمک حلقه فور تک تک کاراکترهای متن بالا را نمایش دهید
+# با کمک حلقه وایل تک تک کاراکترهای متن بالا را نمایش دهید
+# با کمک حلقه فور تک تک کاراکترهای انگلیسی متن بالا را نمایش دهید
